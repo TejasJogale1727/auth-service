80dfcc96d64498d3f2ee2ff0d9c2f42e587904f8
tejas.jogale <tejas.jogale@vertoz.com>
2025-08-07 19:59:57 +0530

    Minor changes

--- src/main/java/com/docuguard/auth_service/controller/AuthController.java
+++ src/main/java/com/docuguard/auth_service/controller/AuthController.java
@@ -13,7 +13,7 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory; 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
